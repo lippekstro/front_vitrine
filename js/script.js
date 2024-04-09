7098e11e88efa0c8b88abdeba9d8a02457fde9c5
lippekstro <fellipecastro13@hotmail.com>
2024-04-09 09:44:43 -0300

    ajuste no icone do tema claro escuro

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const btn_modo = document.querySelector('.modo')
 const html = document.querySelector('html')
+const icone = document.querySelector('.modo span')
 
 let isDark = false
 let modo = []
@@ -8,10 +9,12 @@ btn_modo.addEventListener('click', ()=>{
     if(isDark){
         html.setAttribute('data-bs-theme', 'light')
         localStorage.setItem('modo', 'claro')
+        icone.innerHTML = 'dark_mode'
         isDark = false
     } else {
         html.setAttribute('data-bs-theme', 'dark')
         localStorage.setItem('modo', 'escuro')
+        icone.innerHTML = 'light_mode'
         isDark = true
     }
     
@@ -22,8 +25,10 @@ let valor = localStorage.getItem('modo')
 function trocaTemaAuto(valor){
     if(valor == 'escuro'){
         html.setAttribute('data-bs-theme', 'dark')
+        icone.innerHTML = 'light_mode'
     } else {
         html.setAttribute('data-bs-theme', 'light')
+        icone.innerHTML = 'dark_mode'
     }
 }
 
