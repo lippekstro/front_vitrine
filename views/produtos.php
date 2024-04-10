d7233694aa32ce06e876940d658f3b66ab848eb6
lippekstro <fellipecastro13@hotmail.com>
2024-04-10 17:40:59 -0300

    ajuste na exibicao dos produtos em grid

--- views/produtos.php
+++ views/produtos.php
@@ -2,7 +2,7 @@
 require_once "../templates/_cabecalho.php";
 ?>
 
-<section class="m-3 d-flex justify-content-evenly align-items-center flex-wrap cards">
+<section class="m-3 d-grid justify-content-evenly align-items-center cards grid-template">
     <?php for ($j = 0; $j < 9; $j++) : ?>
         <div class="card col-sm-12 m-3" style="width: 18rem;">
             <img src="https://source.unsplash.com/random/1920x1080/?product" class="card-img-top" alt="...">
