1b34d6bf7c3af3b4483bb5ffb9231d5a6b26f7ab
lippekstro <fellipecastro13@hotmail.com>
2024-04-17 08:26:19 -0300

    modelamos o banco

--- db/bd.sql
+++ db/bd.sql
@@ -1,1 +1,27 @@
-aqui ficam as configuracoes de conexao com o banco e a estrutura dele
+CREATE DATABASE tarde_vitrine;
+
+
+CREATE TABLE usuarios(
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome_usuario VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    foto_usuario LONGBLOB,
+    tipo_usuario INT DEFAULT 1
+);
+
+CREATE TABLE categorias(
+    id_categoria INT PRIMARY KEY AUTO_INCREMENT,
+    nome_categoria VARCHAR(255) NOT NULL,
+    foto_categoria LONGBLOB
+);
+
+CREATE TABLE produtos(
+    id_produto INT PRIMARY KEY AUTO_INCREMENT,
+    nome_produto VARCHAR(255) NOT NULL,
+    preco DECIMAL(8,2) NOT NULL,
+    descricao TEXT,
+    foto_produto LONGBLOB,
+    id_categoria INT,
+    FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
+);
